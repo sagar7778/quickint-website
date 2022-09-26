a4b0b3595ac5ebd49e4b093f34c4fb2318a82caa
Neel Patel <neelpatel.6531@gmail.com>
2022-09-26 12:56:35 +0530

    add portfolio pages

--- src/components/Project-details-description/index.jsx
+++ src/components/Project-details-description/index.jsx
@@ -15,10 +15,24 @@ const ProjectDetailsDescription = () => {
           <div className="col-lg-8 offset-lg-1 col-md-8 mb-30">
             <div className="text">
               <p className="extra-text">
-                We are a Creative Agency &amp; Startup Studio that provides Digital
-                Products and Services turns to focus on client success. We
-                specialize in user interface design, including front-end
-                development which we consider to be an integral part.
+                Mitti is an IoT platform which provides sustainable and reliable
+                solutions to minimize uncertainty of yield production in
+                agriculture. Until the invention of mitti, farmers from south
+                Asia, Brazil and other parts of the world didn’t have access and
+                knowledge to control the most important factors which crops need
+                in-check for their proper growth. Mitti is an Iot device and
+                mobile application that helps farmers to measure,monitor and
+                control soil moisture, humidity, nutrients requirements, air
+                pollution, solar radiation which further process through
+                algorithms developed by our deep research and consultation with
+                several agro experts. The device is one time investment as it is
+                a maintenance free product and powered by solar energy. On the
+                other hand, Mitti app provides assistance to farmers about water
+                and fertilizer requirements for different crops using device to
+                optimize resources and get state of the art crop quality and
+                maximum yield from the agricultural land. Main feature of this
+                device is that it provides real time data which farmers can
+                experience in their regional language.
               </p>
             </div>
           </div>
