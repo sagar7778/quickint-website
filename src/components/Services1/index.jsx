2dba7131a6ccef81913d1891c7a14c07820c5f7e
Priyanka <original.manaria@gmail.com>
2022-09-27 17:13:58 +0530

    make home page responsive

--- src/components/Services1/index.jsx
+++ src/components/Services1/index.jsx
@@ -21,7 +21,10 @@ const Services1 = () => {
           <div
             className="col-lg-3 col-md-6 item-box bg-img wow fadeInLeft"
             data-wow-delay=".3s"
-            style={{ backgroundImage: "url(/img/1.jpg)" }}
+            style={{
+              backgroundImage: "url(/img/1.jpg)",
+              width: "-webkit-fill-available",
+            }}
           >
             <h4 className="custom-font">
               Best Of <br /> Our Services
@@ -39,7 +42,11 @@ const Services1 = () => {
             <span className="icon pe-7s-phone"></span>
             <h6>Web and Mobile app devlopment</h6>
             <p>
-            Digitize your business with our custom, user-friendly and state-of-the-art web or mobile software. In which we can provide you with operational efficiency by utilizing the latest technologies that give a boost to your business.
+              Digitize your business with our custom, user-friendly
+              and state-of-the-art web or mobile software. In which we
+              can provide you with operational efficiency by utilizing
+              the latest technologies that give a boost to your
+              business.
             </p>
           </div>
           <div
@@ -47,11 +54,14 @@ const Services1 = () => {
             data-wow-delay=".7s"
           >
             <span className="icon pe-7s-phone"></span>
-            <h6>
-              DevOps auutomation
-            </h6>
-            <p> 
-              <br></br>Let’s combine your business with our software and shorten the software development life cycle (SDLC). Which ultimately helps you to shift your typical traditional business methods into the new digital generation ones.</p>
+            <h6>DevOps auutomation</h6>
+            <p>
+              <br></br>Let’s combine your business with our software
+              and shorten the software development life cycle (SDLC).
+              Which ultimately helps you to shift your typical
+              traditional business methods into the new digital
+              generation ones.
+            </p>
           </div>
           <div
             className="col-lg-3 col-md-6 item-box wow fadeInLeft"
@@ -59,9 +69,13 @@ const Services1 = () => {
           >
             <span className="icon pe-7s-display1"></span>
             <h6>
-            Manual testing and quality assurance (QA) <br />
+              Manual testing and quality assurance (QA) <br />
             </h6>
-            <p>Let our quality analysts check your existing or newly developed digital service, to ensure you services with least bugs and high quality codes.</p>
+            <p>
+              Let our quality analysts check your existing or newly
+              developed digital service, to ensure you services with
+              least bugs and high quality codes.
+            </p>
           </div>
         </div>
       </div>
